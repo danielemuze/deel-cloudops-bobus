0a197544668a9168447f3bf3ba28e1c1f836715a
danielemuze <danielemuze@gmail.com>
2024-06-30 20:59:30 +0100

    added the right port to the backend of the LB

--- elb.tf
+++ elb.tf
@@ -21,7 +21,7 @@ resource "aws_lb_listener" "web" {
 
 resource "aws_lb_target_group" "web" {
   name     = "web-targets"
-  port     = 80
+  port     = 5000
   protocol = "HTTP"
   vpc_id   = aws_vpc.main.id
 
