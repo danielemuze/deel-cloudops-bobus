6c784b144be65df2b4fb06265a0d25c7fc8db1b6
danielemuze <danielemuze@gmail.com>
2024-06-30 20:41:30 +0100

    Added the port of the container in the security group.

--- security_groups.tf
+++ security_groups.tf
@@ -22,6 +22,12 @@ resource "aws_security_group" "web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 5000
+    to_port     = 5000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
   egress {
     from_port   = 0
     to_port     = 0
