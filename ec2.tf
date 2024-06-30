3e4d2ea555eb2e18232d416317fe105cc74cabd6
danielemuze <danielemuze@gmail.com>
2024-06-30 20:19:30 +0100

    changed publuc key to be used

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,6 @@
 resource "aws_key_pair" "deployer" {
   key_name   = "deployer-key"
-  public_key = "your_public_key_here"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCJhXZfxqfLCU5taYdVCs817lHSQdE+F3qtCYhN5BwEmKzgIvVX6ibSNFGT3R3/GAYbJFbra4gFAwO0PT+B5uRREGhnkaU2pzrHYwMlwrF4HHckzLnBwnlluangmxru/VFQn8+ypOm6xMp+1rn8R506KDavqnraszx9S9a42lbfAm+9kAklXeldiWy85y6X2sO+w7JS6tB9glDRtboyqmfYrDrwewaUJaSWXM/ePOzyRBgO1HnalfdPj3tLvPlPQzTC2WIgwm5NG/KKm33sJInN0aJ9zaTDW00YljzZwVlW1a3+nt2eYlKq3/1+Uoffcn+/braz6o7VFdOgHOw1cB9D deployer-key"
 }
 resource "aws_launch_template" "web" {
   name_prefix       = "web-launch-configuration-"
